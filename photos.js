9c88b1cf2ec219d93c52b0eb90884096c8ea70b5
Marcelo Itacarambi <66660861+ItacarambiSec@users.noreply.github.com>
2025-09-01 19:43:09 -0300

    Create photos.js

---
+++ photos.js
@@ -0,0 +1,9 @@
+// photos.js
+// Esta lista contém os nomes dos arquivos de imagem para o fundo do site.
+// A ordem das fotos define a sequência em que elas serão exibidas, uma a cada dia.
+export const photos = [
+  '20241027_195616 (2).jpg',
+  // Adicione todos os nomes dos seus arquivos de imagem aqui.
+  'foto2.jpg',
+  'foto3.jpg'
+];
